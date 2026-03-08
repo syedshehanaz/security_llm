ce805f4698c13ef9ae7aaab8d6c9320def2d9edd
Syed shehanaz <syedshehanaz123@gmail.com>
2026-03-08 15:23:18 +0530

    Update web_ui.py

--- web_ui.py
+++ web_ui.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from agents.coordinator import run_coordinator
 
-st.title("🔐 Cybersecurity LLM Agents Dashboard")
+st.title(" Cybersecurity LLM Agents Dashboard")
 
 if st.button("Run Coordinator"):
     st.write("Running...")
     result = run_coordinator()
     st.success(result)
+
